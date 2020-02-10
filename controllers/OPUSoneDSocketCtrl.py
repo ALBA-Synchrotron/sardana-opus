810da53436e0d8adddd9a413a3b8b34f144d6374
cfalcon <cfalcon@cells.es>
2020-02-10 17:13:02 +0100

    Improve the 1d ctrl

--- controllers/OPUSoneDSocketCtrl.py
+++ controllers/OPUSoneDSocketCtrl.py
@@ -17,6 +17,10 @@ class OPUSoneDSocketCtrl(OneDController, Referable):
                Description: 'Opus Ds URI',
                DefaultValue: "bl01/ct/opus"
                },
+        "linkam_ds": {Type: str,
+               Description: 'Linkam Ds URI',
+               DefaultValue: "bl01/ct/linkam"
+               },
     }
 
     axis_attributes = {
@@ -44,10 +48,20 @@ class OPUSoneDSocketCtrl(OneDController, Referable):
                       Description: 'Add linkam temp to filename',
                       Access: DataAccess.ReadWrite
                       },
+        "opus_mode": {Type: int,
+                      Description: 'Acq mode (0: IR, 1:VISIBLE)',
+                      Access: DataAccess.ReadWrite
+                      },
+        "opus_cam_intensity": {Type: int,
+                      Description: 'Light intensity (for visible mode) 0 to 100',
+                      Access: DataAccess.ReadWrite
+                      },
     }
 
     ON = 1
     MOVING = 0
+    IR = 0
+    VISIBLE = 1
 
     def __init__(self, inst, props, *args, **kwargs):
         super().__init__(inst, props, *args, **kwargs)
@@ -66,9 +80,11 @@ class OPUSoneDSocketCtrl(OneDController, Referable):
         self._opus_exp = None
         self._opus_xpp = None
         self._add_temp2filename = False
+        self._opus_mode = 0
+        self._opus_cam_intensity = 100
         
         try:
-            self.linkam = PyTango.DeviceProxy('bl01/ct/linkam')
+            self.linkam = PyTango.DeviceProxy(self.linkam_ds)
         except:
             self.linkam = None
 
@@ -76,7 +92,6 @@ class OPUSoneDSocketCtrl(OneDController, Referable):
         self._log.debug("ReadOne... {0}".format(self._state))
         value = None
         if self._opusds.state() is PyTango.DevState.ON:
-            # TODO: try to return reference
             try:
                 output = self._opusds.getLastOpusOutput()
                 self._log.debug("cmd output: {0}".format(output))
@@ -85,14 +100,9 @@ class OPUSoneDSocketCtrl(OneDController, Referable):
         return value
 
     def RefOne(self, axis):
-        temp_name = ''
-        if self._add_temp2filename and self.linkam:
-            temp_name = "_Temp{:+07.2f}".format(self._linkam_temp).replace('.', '_')
-
-        name = '{0}{1}'.format(self._opus_nam, temp_name)
+        name = '{0}{1}.0'.format(self._opus_nam, self._temp_name)
         fullpath = os.path.join(self._opus_pth, name)
-        scheme = 'file'
-        return '{}://{}'.format(scheme, fullpath)
+        return 'file://{}'.format(fullpath)
 
     def StateOne(self, ind):
         self._log.debug("StateOne...")
@@ -115,11 +125,11 @@ class OPUSoneDSocketCtrl(OneDController, Referable):
         # self._log.debug('PreStartOne axis %s' % axis)
         self._opus_cmd = "COMMAND_LINE MeasureSample (0, {{EXP='{0}', XPP='{1}'"
         if self._opus_nam != '':
-            temp_name = ''
+            self._temp_name = ''
             if self._add_temp2filename and self.linkam:
                 self._linkam_temp = self.linkam.read_attribute("temperature").value
-                temp_name = "_Temp{:+07.2f}".format(self._linkam_temp).replace('.', '_')
-            self._opus_cmd += ", NAM='{0}{1}'".format(self._opus_nam, temp_name)
+                self._temp_name = "_Temp{:+07.2f}".format(self._linkam_temp).replace('.', '_')
+            self._opus_cmd += ", NAM='{0}{1}'".format(self._opus_nam, self._temp_name)
                 
         if self._opus_pth != '':
             self._opus_cmd += ", PTH='{0}'".format(self._opus_pth)
@@ -132,8 +142,14 @@ class OPUSoneDSocketCtrl(OneDController, Referable):
     def StartOne(self, axis, value=None):
         self._log.debug("StartOne")
         self._opus_macro_is_running = True
-        self._opusds.runOpusCMD(self._opus_cmd)
-
+        if self._opus_mode == self.IR:
+            self._opusds.runOpusCMD(self._opus_cmd)
+        elif self._opus_mode == self.VISIBLE:
+            # Take sanpshot
+            cmd = 'take_snapshot {} {}'.format(self._opus_pth,
+                                               '{0}{1}'.format(self._opus_nam, self._temp_name))
+            self._opusds.runOpusCMD(cmd)
+            
     def LoadOne(self, ind, value):
         pass
 
@@ -155,6 +171,10 @@ class OPUSoneDSocketCtrl(OneDController, Referable):
             return self._opus_nam
         elif name.lower() == "add_temp2filename":
             return self._add_temp2filename
+        elif name.lower() == "opus_mode":
+            return self._opus_mode
+        elif name.lower() == "opus_cam_intensity":
+            return self._opus_cam_intensity
 
     def SetAxisExtraPar(self, axis, name, value):
         if name.lower() == "ds":
@@ -169,5 +189,32 @@ class OPUSoneDSocketCtrl(OneDController, Referable):
             self._opus_nam = value
         elif name.lower() == "add_temp2filename":
             self._add_temp2filename = value
+        elif name.lower() == "opus_mode":
+            self._opus_mode = value
+            if value == self.IR:
+                # Move to IR
+                cmd = "COMMAND_LINE SendCommand(0,+{UNI='MOT56=1'});"
+                self._opusds.runOpusCMDSync(cmd)                
+            elif value == self.VISIBLE:
+                # Move to Visible
+                cmd = "COMMAND_LINE SendCommand(0,+{UNI='MOT56=2'});"
+                self._opusds.runOpusCMDSync(cmd)
+                # Change light intensity 
+                lintensity = 100 + self._opus_cam_intensity
+                cmd = "COMMAND_LINE SendCommand(0,+{{UNI='MOT56={}'}});".format(lintensity)
+                self._opusds.runOpusCMDSync(cmd)
+        elif name.lower() == "opus_cam_intensity":
+            self._opus_cam_intensity = value
+            # Change light intensity 
+            lintensity = 100 + self._opus_cam_intensity
+            if self._opus_mode == self.VISIBLE
+                cmd = "COMMAND_LINE SendCommand(0,+{{UNI='MOT56={}'}});".format(lintensity)
+                self._opusds.runOpusCMDSync(cmd)
+
+    #def SetAxisPar(self, axis, parameter, value):
+        #if parameter == "value_ref_pattern":
+            #self._value_ref_pattern = value
+        #elif parameter == "value_ref_enabled":
+            #self._value_ref_enabled = value
 
 
